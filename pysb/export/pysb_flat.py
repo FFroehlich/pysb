ea13b22bd4e2f72aee7bf16c65e4b49e59efe728
FFroehlich <fabian@schaluck.com>
2019-02-08 15:59:52 -0500

    improve pysb_flat export

--- pysb/export/pysb_flat.py
+++ pysb/export/pysb_flat.py
@@ -74,10 +74,7 @@ class PysbFlatExporter(Exporter):
         write_cset(self.model.observables)
         write_cset(self.model.expressions_dynamic())
         write_cset(self.model.rules)
-        for ep in self.model.energypatterns:
-            output.write("EnergyPattern('%s', %s, %s)\n" %
-                         (ep.name, repr(ep.pattern), ep.energy.name))
-        output.write("\n")
+        write_cset(self.model.energypatterns)
         for pattern, value in self.model.initial_conditions:
             output.write("Initial(%s, %s)\n" % (repr(pattern), value.name))
         output.write("\n")
